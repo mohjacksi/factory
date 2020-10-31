3196fb276e5cc5472af4240db6d5eef52ebf596a
Mohammed Jacksi <moh.jacksi@gmail.com>
2020-10-31 18:01:19 +0300

    .

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
-Route::group(['prefix' => 'v1', 'as' => 'api.', 'namespace' => 'Api\V1\Admin', 'middleware' => ['auth:api']], function () {
+Route::group(['prefix' => 'v1', 'as' => 'api.', 'namespace' => 'Api\V1\Admin',
+ //'middleware' => ['auth:api']],
+  function () {
     // Permissions
     Route::apiResource('permissions', 'PermissionsApiController');
 
