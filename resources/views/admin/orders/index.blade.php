191666701f2b6b9cf253d47b05a1c37e701add48
mostafa hamed <mostafa.hamed1944@gmail.com>
2021-02-19 17:16:42 +0200

    dummy test

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -38,6 +38,7 @@
                             {{ trans('cruds.order.fields.subtotal') }}
                         </th>
                         <th>
+
                             {{ trans('cruds.order.fields.discount') }}
                         </th>
                         <th>
